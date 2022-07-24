eb804e51d64d34714447b3b8d4efeaa0a5c9cc08
Vitor.Gomes <santosgomesv@gmail.com>
2022-07-24 19:17:24 -0300

    primeiras configuraçoes

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS',default=[], cast=Csv())
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
